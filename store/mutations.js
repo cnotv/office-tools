f6099887c099efff83aba42007526d48b2ab1428
cnotv <contact@cnotv.xyz>
2018-02-05 22:09:12 +0100

    removed conditional in favor of dynamic object access

--- store/mutations.js
+++ store/mutations.js
@@ -5,19 +5,7 @@ export default {
   },
 
   SWITCH_OPTION: (state, store) => {
-    if (store === 'clipped') {
-      state.global.clipped = !state.global.clipped
-    } else if (store === 'drawer') {
-      state.global.drawer = !state.global.drawer
-    } else if (store === 'miniVariant') {
-      state.global.miniVariant = !state.global.miniVariant
-    } else if (store === 'fixed') {
-      state.global.fixed = !state.global.fixed
-    } else if (store === 'account') {
-      state.global.account = !state.global.account
-    } else if (store === 'dialog') {
-      state.global.dialog = !state.global.dialog
-    }
+    state.global[store] = !state.global[store]
   },
 
   UPDATE_SETTINGS: (state, fields) => {
@@ -26,38 +14,15 @@ export default {
   },
 
   NEW_FILE: (state, payload) => {
-    let array
-    if (payload.type === 'cv') {
-      array = state.page.cv.files
-      state.page.cv.counter++
-    } else if (payload.type === 'ocr') {
-      array = state.page.ocr.files
-      state.page.ocr.counter++
-    } else if (payload.type === 'invoice') {
-      array = state.page.invoice.files
-      state.page.invoice.counter++
-    }
-    array.push(payload.file)
+    state.page[payload.type].files.push(payload.file)
+    state.page[payload.type].counter++
   },
 
   UPDATE_FILE: (state, payload) => {
-    if (payload.type === 'cv') {
-      state.page.cv.files[payload.id] = payload.file
-    } else if (payload.type === 'ocr') {
-      state.page.ocr.files[payload.id] = payload.file
-    } else if (payload.type === 'invoice') {
-      state.page.invoice.files[payload.id] = payload.file
-    }
+    state.page[payload.type].files[payload.id] = payload.file
   },
 
   REMOVE_FILE: (state, payload) => {
-    if (payload.type === 'cv') {
-      // state.page.cv.files = state.page.cv.files.filter(file => file.id !== parseInt(payload.id))
-      state.page.cv.files[payload.id - 1] = null
-    } else if (payload.type === 'ocr') {
-      state.page.ocr.files[payload.id - 1] = null
-    } else if (payload.type === 'invoice') {
-      state.page.invoice.files[payload.id - 1] = null
-    }
+    state.page[payload.type].files[payload.id - 1] = null
   }
 }
