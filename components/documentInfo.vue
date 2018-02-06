5a14452e5d1b5250d8ef26113f1182958e15db9d
cnotv <contact@cnotv.xyz>
2018-02-06 15:03:22 +0100

    added document info component, parent with global content fallback

---
+++ components/documentInfo.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card-text>
+    <p
+      v-for="(info, i) in infos"
+    >{{ info.value || getSettings.settings[i].value || '' }}</p>
+  </v-card-text>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  props: ['infos'],
+  computed: {
+    ...mapGetters([
+      'getSettings'
+    ])
+  }
+}
+</script>
+
+<style>
+</style>
